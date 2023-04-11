eeb76d9719d86e34ebc9dae48ce26df1716b54a9
Pavel <pmaskarev@vk.com>
2023-04-11 08:50:13 +0300

    Added task041

---
+++ Task041/Program.cs
@@ -0,0 +1,43 @@
+﻿int Print(string arg)
+{
+    System.Console.WriteLine($"Введите {arg}");
+    int Arg = int.Parse(Console.ReadLine());
+    return Arg;
+}
+
+
+int [] CreateArray(int Length)
+{
+    int [] Array = new int [Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        System.Console.WriteLine($"Введите элемент массива с индексом {i}: ");
+        Array[i] = int.Parse(Console.ReadLine());
+    }
+    return Array;
+}
+
+void PrintArray(int [] Array)
+{
+    System.Console.Write("Полученный массив: [");
+    for (int i = 0; i < Array.Length-1; i++)
+    {
+        System.Console.Write(Array[i]+";");
+    }
+    System.Console.WriteLine(Array[Array.Length-1]+"]");
+}
+
+void MoreThenNull (int[] Array)
+{
+    int Kol = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i]>0) Kol++;
+    }
+    System.Console.WriteLine($"Количество чисел больше 0 в массиве: {Kol}");;
+}
+
+int Len = Print("длину массива: ");
+int [] Mas = CreateArray(Len);
+PrintArray(Mas);
+MoreThenNull(Mas);
